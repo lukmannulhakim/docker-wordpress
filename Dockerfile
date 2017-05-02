b98a6ca5962f1bfe5e64036faaa36f5899833120
Dzikri Aziz <kvcrvt@gmail.com>
2017-05-02 23:41:08 +0800

    Set proper ownership of uploads & upgrade dirs

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ ENV WP_CORE_DIR /usr/src/wordpress
 
 # Install packages from testing repo's
 RUN apk update && apk upgrade && apk add \
-    nginx supervisor curl bash \
+    nginx supervisor curl bash shadow \
     php7 php7-fpm php7-mysqli php7-json php7-openssl php7-curl php7-zlib \
     php7-xml php7-phar php7-intl php7-dom php7-xmlreader php7-ctype \
     php7-mbstring php7-gd php7-opcache && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,55 @@
 # terminate on errors
 set -e
 
-chown -R nobody:nobody /var/www
+cat << "EOF"
+Created by...
+      ___           ___           ___           ___           ___
+     /|  |         /\  \         /\__\         /\  \         /\  \
+    |:|  |         \:\  \       /:/  /        /::\  \        \:\  \         ___
+    |:|  |          \:\  \     /:/  /        /:/\:\__\        \:\  \       /\__\
+  __|:|  |      ___  \:\  \   /:/  /  ___   /:/ /:/  /    ___  \:\  \     /:/  /
+ /\ |:|__|____ /\  \  \:\__\ /:/__/  /\__\ /:/_/:/__/___ /\  \  \:\__\   /:/__/
+ \:\/:::::/__/ \:\  \ /:/  / \:\  \ /:/  / \:\/:::::/  / \:\  \ /:/  /  /::\  \
+  \::/~~/~      \:\  /:/  /   \:\  /:/  /   \::/~~/~~~~   \:\  /:/  /  /:/\:\  \
+   \:\~~\        \:\/:/  /     \:\/:/  /     \:\~~\        \:\/:/  /   \/__\:\  \
+    \:\__\        \::/  /       \::/  /       \:\__\        \::/  /         \:\__\
+     \/__/         \/__/         \/__/         \/__/         \/__/           \/__/
+
+   https://kucrut.org
+
+EOF
+
+# Stolen from https://hub.docker.com/u/binhex/
+echo "[info] System information $(uname -a)"
+
+export PUID=$(echo "${PUID}" | sed -e 's/^[ \t]*//')
+if [[ ! -z "${PUID}" ]]; then
+    echo "[info] PUID defined as '${PUID}'"
+else
+    echo "[warn] PUID not defined (via -e PUID), defaulting to '99'"
+    export PUID="99"
+fi
+
+# set user nobody to specified user id (non unique)
+usermod -o -u "${PUID}" nobody &>/dev/null
+
+export PGID=$(echo "${PGID}" | sed -e 's/^[ \t]*//')
+if [[ ! -z "${PGID}" ]]; then
+    echo "[info] PGID defined as '${PGID}'"
+else
+    echo "[warn] PGID not defined (via -e PGID), defaulting to '100'"
+    export PGID="100"
+fi
+
+# set group nobody to specified group id (non unique)
+groupmod -o -g "${PGID}" nobody &>/dev/null
+
+mkdir -p /var/www/content/{uploads,upgrade}
+
+if [[ ! -f "/var/www/content/perms.txt" ]]; then
+    echo "Setting uploads & upgrade directories permissions..."
+    chown -R nobody:nobody /var/www/content/{uploads,upgrade}
+    echo "Permission set to ${PUID}:${PGID}" > /var/www/content/perms.txt
+fi
+
 exec "$@"
