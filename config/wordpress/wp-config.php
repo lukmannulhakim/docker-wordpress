e5611d2cab3e13bd1e214f982cec70c40deac31c
Dzikri Aziz <kvcrvt@gmail.com>
2017-05-29 05:01:50 +0800

    WP config: Disable WP Cron

--- config/wordpress/wp-config.php
+++ config/wordpress/wp-config.php
@@ -67,4 +67,6 @@ if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === $_SERVER['HTTP_X
 	$_SERVER['HTTPS'] = 'on';
 }
 
+define( 'DISABLE_WP_CRON', true );
+
 require_once ABSPATH . 'wp-settings.php';
