90a06681d30f3506619b88a2eb5e8115f15940d8
Dzikri Aziz <kvcrvt@gmail.com>
2017-05-29 04:58:21 +0800

    Entrypoint: Wait until DB is ready before starting services

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,19 @@ https://kucrut.org
 
 EOF
 
+mysql=( mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASSWORD} --skip-column-names )
+
+# Stolen from XWP's wp-docker.
+function is_db_up() {
+  result=$( echo "SHOW DATABASES LIKE '${DB_NAME}'" | "${mysql[@]}" 2>/dev/null )
+
+  if [ "$result" == "${DB_NAME}" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 # Stolen from https://hub.docker.com/u/binhex/
 echo "[info] System information $(uname -a)"
 
@@ -46,5 +59,12 @@ if [ ! -f "/var/www/wp-content/perms.txt" ]; then
     chown -R nginx:nginx /var/www/wp-content/{uploads,upgrade}
     echo "Permission set." > /var/www/wp-content/perms.txt
 fi
+
+until is_db_up; do
+   echo "Waiting for database to become available..."
+   sleep 3
+done
+
+echo "Database is available. Starting services..."
 
 exec "$@"
